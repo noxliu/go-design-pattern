19cd9bba244c344242b406ee2f1b8060391de7a4
LiuLi <nox60@hotmail.com>
2019-11-26 18:07:11 +0800

    format visitor pattern code

--- visitor/visitor/visitEngine.go
+++ visitor/visitor/visitEngine.go
@@ -1,11 +1,8 @@
 package visitor
 
-import "fmt"
-
 type VisitEngine struct {
 }
 
 func (*VisitEngine) VisitEngine(engine Engine) string {
-	fmt.Println(engine.EngineType)
-	return "visit engine"
+	return engine.EngineType
 }
