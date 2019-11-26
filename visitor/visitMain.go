cb6b865da22fbdaaae6ed5cf3a43da0cab3fecf9
LiuLi <nox60@hotmail.com>
2019-11-26 15:40:52 +0800

    format visitor pattern code

--- visitor/visitMain.go
+++ visitor/visitMain.go
@@ -5,10 +5,21 @@ import (
 )
 
 func main() {
-	wheel := Wheel{WheelSize: 100}
-	engine := Engine{EngineType: "turbo"}
-	carBody := Body{BodyColor: "green", Size: 5}
-	car := Car{Wheel: wheel, Engine: engine, Body: carBody}
+	wheel := Wheel{
+		WheelSize: 100,
+	}
+	engine := Engine{
+		EngineType: "turbo",
+	}
+	carBody := Body{
+		BodyColor: "green",
+		Size:      5,
+	}
+	car := Car{
+		Wheel:  wheel,
+		Engine: engine,
+		Body:   carBody,
+	}
 
 	visitorWheel := VisitWheel{}
 	visitEngine := VisitEngine{}
