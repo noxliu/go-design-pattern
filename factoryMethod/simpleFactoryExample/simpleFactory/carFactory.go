7fe1fb5e2870006e86cd4cb1381a55bbc17fb22a
LiuLi <nox60@hotmail.com>
2019-11-29 20:15:15 +0800

    add template pattern

--- factoryMethod/simpleFactoryExample/simpleFactory/carFactory.go
+++ factoryMethod/simpleFactoryExample/simpleFactory/carFactory.go
@@ -2,12 +2,13 @@ package simpleFactory
 
 func getCar(carType string) Car {
 	if carType == "benz" {
-		car := CarType{
-			carName: carType,
-			size:    10,
-		}
-
-		return car
+		/*
+			car := CarType{
+				carName: carType,
+				size:    10,
+			}
+		*/
+		return nil
 	} else {
 		return nil
 	}
