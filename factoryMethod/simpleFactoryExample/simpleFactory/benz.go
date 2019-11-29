a4db5e50e2ecd60e42b57806c0a46814cd435d65
Li.Liu <nox60@hotmail.com>
2019-11-29 23:20:00 +0800

    Add factory method pattern

--- factoryMethod/simpleFactoryExample/simpleFactory/benz.go
+++ factoryMethod/simpleFactoryExample/simpleFactory/benz.go
@@ -1,15 +1,9 @@
 package simpleFactory
 
-import "fmt"
-
 type Benz struct {
 	CarType CarType
 }
 
 func (b Benz) Drive() string {
 	return b.CarType.carName
-}
-
-func (c Benz) ShowMyCar() {
-	fmt.Println(c.CarType)
 }
--- factoryMethod/simpleFactoryExample/simpleFactory/bmw.go
+++ factoryMethod/simpleFactoryExample/simpleFactory/bmw.go
@@ -1,15 +1,9 @@
 package simpleFactory
 
-import "fmt"
-
 type Bmw struct {
 	CarType CarType
 }
 
 func (b Bmw) Drive() string {
 	return b.CarType.carName
-}
-
-func (c Bmw) ShowMyCar() {
-	fmt.Println(c.CarType)
 }
