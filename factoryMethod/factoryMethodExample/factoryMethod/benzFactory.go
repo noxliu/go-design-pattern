2e0ba965317dba917024b3b182e21598a1fbf72f
liuli <nox60@hotmail.com>
2019-12-02 09:33:10 +0800

    add benz factory

--- factoryMethod/factoryMethodExample/factoryMethod/benzFactory.go
+++ factoryMethod/factoryMethodExample/factoryMethod/benzFactory.go
@@ -1,1 +1,18 @@
 package factoryMethod
+
+type BenzFactory struct {
+}
+
+func (*BenzFactory) GetCar(carName string) Car {
+	cartype := CarType{
+		carName: carName,
+		size:    10,
+	}
+
+	benz := Benz{
+		CarType: cartype,
+	}
+
+	return benz
+
+}
