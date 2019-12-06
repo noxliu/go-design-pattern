b7f82ba0257c32e0127acc72a50201f8bb551def
LiuLi <nox60@hotmail.com>
2019-12-06 17:26:44 +0800

    update state pattern

--- builder/compterBuilderExample/computerBuilder/builder.go
+++ builder/compterBuilderExample/computerBuilder/builder.go
@@ -1,1 +1,26 @@
 package computerBuilder
+
+type ComputerBuilderImplement struct {
+	ComputerItems ComputerItems
+}
+
+func (c ComputerBuilderImplement) SetCpu(cpu string) ComputerBuilder {
+
+	return nil
+}
+
+func (c ComputerBuilderImplement) SetMemory(memory string) ComputerBuilder {
+	return nil
+}
+
+func (c ComputerBuilderImplement) SetHardDisk(hardDisk string) ComputerBuilder {
+	return nil
+}
+
+func (c ComputerBuilderImplement) SetScreenSize(screenSize int) ComputerBuilder {
+	return nil
+}
+
+func (c ComputerBuilderImplement) GetComputerProduct() string {
+	return nil
+}
