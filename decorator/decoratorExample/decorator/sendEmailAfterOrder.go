4a5ceb1542e3d72c75e7701d74489542c29dc550
Li.Liu <nox60@hotmail.com>
2019-12-07 22:55:34 +0800

    update state pattern

--- decorator/decoratorExample/decorator/sendEmailAfterOrder.go
+++ decorator/decoratorExample/decorator/sendEmailAfterOrder.go
@@ -1,1 +1,16 @@
 package decorator
+
+import "fmt"
+
+type SendEmailAfterOrder struct {
+	Function PlaceOrderI
+}
+
+func (s *SendEmailAfterOrder) PlaceOrder(order Order) {
+	sendEmail()
+	s.Function.PlaceOrder(order)
+}
+
+func sendEmail() {
+	fmt.Println("send email...")
+}
