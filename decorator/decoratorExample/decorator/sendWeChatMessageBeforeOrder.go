34bf88998982eeb15f530d4cfd4b1ae36761bc21
Li.Liu <nox60@hotmail.com>
2019-12-08 22:17:12 +0800

    update state pattern

--- decorator/decoratorExample/decorator/sendWeChatMessageBeforeOrder.go
+++ decorator/decoratorExample/decorator/sendWeChatMessageBeforeOrder.go
@@ -1,1 +1,16 @@
 package decorator
+
+import "fmt"
+
+type SendWeChatMessageBeforeOrder struct {
+	Function OrderTicketI
+}
+
+func (s *SendWeChatMessageBeforeOrder) PlaceOrder(order Order) {
+	sendWeChatMessage()
+	s.Function.PlaceOrder(order)
+}
+
+func sendWeChatMessage() {
+	fmt.Println("发送微信信息")
+}
--- decorator/decoratorExample/decoratorMain.go
+++ decorator/decoratorExample/decoratorMain.go
@@ -8,9 +8,15 @@ func main() {
 		ProductId: "09988779",
 	}
 	placeOrder := OrderTicketImplement{}
+
+	//使用短信发送功能包装原有的订票功能
 	sendMsgBeforeOrder := SendMsgBeforeOrder{&placeOrder}
 	sendMsgBeforeOrder.Function = &placeOrder
+
+	//使用邮件发送功能包装上面的短信发送功能（短信发送功能已经包装了原有的订票功能），这样一来，就会实现先发送短信，然后订票，然后发送邮件
 	sendEmailAfterOrder := SendEmailAfterOrder{}
 	sendEmailAfterOrder.Function = &sendMsgBeforeOrder
+
 	sendEmailAfterOrder.PlaceOrder(order)
+
 }
