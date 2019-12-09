4a0758dfce410c1b605bb6e5508ce50d2ebc9b10
LiuLi <nox60@hotmail.com>
2019-12-09 13:52:20 +0800

    update state pattern

--- observer/observerExample/observerMain.go
+++ observer/observerExample/observerMain.go
@@ -6,13 +6,13 @@ import (
 
 func main() {
 	subject := NewSubject()
-	reader1 := NewReader("reader1")
-	reader2 := NewReader("reader2")
-	reader3 := NewReader("reader3")
+	reader1 := NewObserver("reader1")
+	reader2 := NewObserver("reader2")
+	reader3 := NewObserver("reader3")
 
 	subject.Register(*reader1)
 	subject.Register(*reader2)
 	subject.Register(*reader3)
 
-	subject.UpdateContext("Message from subject")
+	subject.UpdateContext("来自Subject的消息，数据处理完成")
 }
