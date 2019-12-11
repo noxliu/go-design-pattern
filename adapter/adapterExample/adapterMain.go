d47a85fd50f31ddf0820cea4686f0ff9e372164c
LiuLi <nox60@hotmail.com>
2019-12-11 17:11:34 +0800

    refactory interface

--- adapter/adapterExample/adapterMain.go
+++ adapter/adapterExample/adapterMain.go
@@ -5,18 +5,18 @@ import (
 )
 
 func main() {
+	//只支持字符串类型的打印工具
 	printer := StringPrinter{}
+
 	stringGenerator := StringGenerator{}
-	printer.PrintString(&stringGenerator)
 
 	intGenerator := IntGenerator{}
-	adapter := IntToStringAdapter{}
-	adapter.IntValue = intGenerator.IntGenerator()
+	adapter := IntToStringAdapter{intGenerator.IntGenerator()}
 
 	intArrayGenerator := GeneratorForIntArray{}
-	adapterArray := ArrayToStringAdapter{}
-	adapterArray.IntArray = intArrayGenerator.IntArrayGenerator()
+	adapterArray := ArrayToStringAdapter{intArrayGenerator.IntArrayGenerator()}
 
+	printer.PrintString(&stringGenerator)
 	printer.PrintString(&adapter)
 	printer.PrintString(&adapterArray)
 
