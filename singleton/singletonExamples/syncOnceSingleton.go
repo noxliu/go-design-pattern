84e8d779e3a392605335079e6dd48079b887aa0b
LiuLi <nox60@hotmail.com>
2019-12-12 10:53:50 +0800

    refactory interface

--- singleton/singletonExamples/syncOnceSingleton.go
+++ singleton/singletonExamples/syncOnceSingleton.go
@@ -1,1 +1,15 @@
 package singletonExamples
+
+import "sync"
+
+type syncOnceSingleton struct{}
+
+var syncOnceInstance *syncOnceSingleton
+var once sync.Once
+
+func GetSyncOnceInstance() *syncOnceSingleton {
+	once.Do(func() {
+		syncOnceInstance = &syncOnceSingleton{}
+	})
+	return syncOnceInstance
+}
