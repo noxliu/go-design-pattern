55eff659e6e9fd7173ef88b112cbd904fef4ebb0
liuli <nox60@hotmail.com>
2019-12-19 15:52:00 +0800

    add chain of responsibility pattern

--- chainOfResponsibility/chainOfResponsibilityExample/chainOfResponsibility/DepartmentManager.go
+++ chainOfResponsibility/chainOfResponsibilityExample/chainOfResponsibility/DepartmentManager.go
@@ -1,1 +1,19 @@
 package chainOfResponsibility
+
+import "fmt"
+
+type DepartmentManager struct {
+}
+
+func (*DepartmentManager) HaveRight(money int) bool {
+	return money < 5000
+}
+
+func (*DepartmentManager) HandleFeeRequest(name string, money int) bool {
+	if name == "tom" {
+		fmt.Printf("Dep manager permit %s %d fee request\n", name, money)
+		return true
+	}
+	fmt.Printf("Dep manager don't permit %s %d fee request\n", name, money)
+	return false
+}
--- chainOfResponsibility/chainOfResponsibilityExample/chainOfResponsibility/GeneralManager.go
+++ chainOfResponsibility/chainOfResponsibilityExample/chainOfResponsibility/GeneralManager.go
@@ -1,1 +1,19 @@
 package chainOfResponsibility
+
+import "fmt"
+
+type GeneralManager struct {
+}
+
+func (*GeneralManager) HaveRight(money int) bool {
+	return true
+}
+
+func (*GeneralManager) HandleFeeRequest(name string, money int) bool {
+	if name == "ada" {
+		fmt.Printf("General manager permit %s %d fee request\n", name, money)
+		return true
+	}
+	fmt.Printf("General manager don't permit %s %d fee request\n", name, money)
+	return false
+}
