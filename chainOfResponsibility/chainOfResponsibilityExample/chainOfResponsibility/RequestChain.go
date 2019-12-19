6be8e4fc49644a1329315a9ee95b1de0ddc24067
liuli <nox60@hotmail.com>
2019-12-19 15:41:48 +0800

    add chain of responsibility pattern

---
+++ chainOfResponsibility/chainOfResponsibilityExample/chainOfResponsibility/RequestChain.go
@@ -0,0 +1,30 @@
+package chainOfResponsibility
+
+//管理者接口
+type Manager interface {
+	HaveRight(money int) bool
+	HandleFeeRequest(name string, money int) bool
+}
+
+type RequestChain struct {
+	Manager
+	successor *RequestChain
+}
+
+func (r *RequestChain) SetSuccessor(m *RequestChain) {
+	r.successor = m
+}
+
+func (r *RequestChain) HandleFeeRequest(name string, money int) bool {
+	if r.Manager.HaveRight(money) {
+		return r.Manager.HandleFeeRequest(name, money)
+	}
+	if r.successor != nil {
+		return r.successor.HandleFeeRequest(name, money)
+	}
+	return false
+}
+
+func (r *RequestChain) HaveRight(money int) bool {
+	return true
+}
