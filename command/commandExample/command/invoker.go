63d632cbeb61ac79acc5466a7266b739c4137683
Liu Li <nox60@hotmail.com>
2020-01-26 23:35:25 +0800

    Update command pattern

--- command/commandExample/command/invoker.go
+++ command/commandExample/command/invoker.go
@@ -13,6 +13,7 @@ func (b *Invoker) AddOrder(command Command) {
 func (b *Invoker) ExecuteOrders() {
 	for _, command := range b.orders {
 		fmt.Println(">>>>处理命令<<<<")
+		fmt.Println("判断库存")
 		command.ExecCommand()
 		fmt.Println("记录日志")
 		fmt.Println(">>>>处理结束<<<<")
